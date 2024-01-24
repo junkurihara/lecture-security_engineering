cd3468244c96c445782d723d3dd9055bbfe1c06e
Jun Kurihara <junkurihara@users.noreply.github.com>
2024-01-24 12:06:46 +0900

    remove karma

--- sample-07-09-rs/start.sh
+++ sample-07-09-rs/start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Build fido2 client lib from TypeScript codes"
-cd ../sample-07-09
-pnpm cleanup && pnpm i && pnpm build && cp dist/*.bundle.js ../sample-07-09-rs/assets/
+cd ./frontend-lib
+pnpm cleanup && pnpm i && pnpm build && cp dist/*.bundle.js ../assets/
 
 echo "Build fido2 server from Rust codes"
-cd ../sample-07-09-rs
+cd ..
 cargo build --release
 
 echo "Run fido2 server"
